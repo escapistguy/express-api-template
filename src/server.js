23c83708b538d4bf09b1b6bbeb0ca56449038824
darana-wizeline <diego.arana@wizeline.com>
2022-02-08 14:53:00 -0600

    Added dotenv setup

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const app = require('./app');
 
+if (process.env.NODE_ENV === 'development') {
+	require('dotenv').config();
+}
+
 async function bootstrap() {
-	const port = process.env.PORT || 8000;
+	const port = process.env.PORT;
 	await app.listen(port);
 
 	console.log(
